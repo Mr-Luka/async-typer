536f89fe97d5f5b3cf8d4e426d6476c5c2b9d32e
Luka <luka.luka.luka93@gmail.com>
2024-05-06 14:42:09 -0700

    finished draw function everything is working

--- scripts.js
+++ scripts.js
@@ -2,14 +2,24 @@ function wait (ms = 0) {
     return new Promise(resolve=> {setTimeout(resolve, ms)});
 };
 
-function getRandomBetween (min, max, randomNumber = Math.random()) {
+function getRandomBetween (min = 20, max = 150, randomNumber = Math.random()) {
     return Math.floor(randomNumber * (max - min) + min);
 }
-
-function draw (element) {
-    console.log(element)
+// async for of loop
+async function draw (el) {
+    const text = el.textContent;
+    let soFar = '';
+    for(const letter of text) {
+        soFar += letter;
+        el.textContent = soFar;
+        // wait for some amount of time
+        const {typeMin, typeMax} = el.dataset;
+        const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+        await wait(amountOfTimeToWait)
+    }
 }
 
-const els = document.querySelectorAll("[data-type");
-
-els.forEach(draw);
+// const els = document.querySelector("[data-type]");
+// els.forEach(element=>{console.log(element)});
+// This is shorter:
+document.querySelectorAll("[data-type]").forEach(draw);
