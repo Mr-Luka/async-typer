8c7b416ae1879311906d621abad4dbc8878b18a4
Luka <luka.luka.luka93@gmail.com>
2024-05-06 14:51:12 -0700

    made draw function, using Recursion

--- scripts.js
+++ scripts.js
@@ -6,17 +6,36 @@ function getRandomBetween (min = 20, max = 150, randomNumber = Math.random()) {
     return Math.floor(randomNumber * (max - min) + min);
 }
 // async for of loop
-async function draw (el) {
+// async function draw (el) {
+//     const text = el.textContent;
+//     let soFar = '';
+//     for(const letter of text) {
+//         soFar += letter;
+//         el.textContent = soFar;
+//         // wait for some amount of time
+//         const {typeMin, typeMax} = el.dataset;
+//         const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+//         await wait(amountOfTimeToWait)
+//     }
+// }
+
+// RECURSION
+function draw(el){
+    let index = 1;
     const text = el.textContent;
-    let soFar = '';
-    for(const letter of text) {
-        soFar += letter;
-        el.textContent = soFar;
-        // wait for some amount of time
-        const {typeMin, typeMax} = el.dataset;
+    const {typeMin, typeMax} = el.dataset;
+    async function drawLetter(){
+        el.textContent = text.slice(0, index)
+        index += 1;
+        // exit condition
         const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
         await wait(amountOfTimeToWait)
+        if(index <= text.length){
+            drawLetter();
+        }
     }
+    // When this function draw() runs, kick off drawLetter
+    drawLetter();
 }
 
 // const els = document.querySelector("[data-type]");
