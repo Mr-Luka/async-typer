650ecdb0ef33cc27f996aede7b4dc6537fb4c04b
Luka <luka.luka.luka93@gmail.com>
2024-05-06 14:32:36 -0700

    nade function draw and looped over elements

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ function wait (ms = 0) {
     return new Promise(resolve=> {setTimeout(resolve, ms)});
 };
 
-function getRandomBetween (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+function getRandomBetween (min, max, randomNumber = Math.random()) {
+    return Math.floor(randomNumber * (max - min) + min);
 }
+
+function draw (element) {
+    console.log(element)
+}
+
+const els = document.querySelectorAll("[data-type");
+
+els.forEach(draw);
