34f0c724d96f74c1d3a23f9e92cbfc68828b43ff
Luka <luka.luka.luka93@gmail.com>
2024-05-06 14:26:09 -0700

    made Get Random number between numbers function

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ function wait (ms = 0) {
     return new Promise(resolve=> {setTimeout(resolve, ms)});
 };
 
+function getRandomBetween (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
